42ccd22bd29e7c9a3479b653de8aa15b099bf4f9
Greg Maza <mazag52@gmail.com>
2013-07-22 19:22:26 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sssblog::Application.config.secret_token = 'e82cabd483f7d0b7bce3d546ff5ed7fa160fc364c541990fec9e9b59bba9d0831b319039c823ff4761a27e94c9ebbf05a41b46374a4a671e6a9fe5b4d3b0f162'
